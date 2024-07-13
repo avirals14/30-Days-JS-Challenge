674af7948c4dd2db28a943975e76f296bd1f8cd8
Aviral Sharma <154816352+avirals14@users.noreply.github.com>
2024-07-13 21:37:33 +0000

    day01 final

--- Day01/program_01.js
+++ Day01/program_01.js
@@ -1,23 +1,23 @@
 // task1
-var v1 = 5;
-console.log(v1);
+var v1 = 5
+console.log(v1)
 
 // task2
-let v2 = "String";
-console.log(v2);
+let v2 = "String"
+console.log(v2)
 
 // task 3
-const b1 = true;
-console.log(b1);
+const b1 = true
+console.log(b1)
 
 // task 4
 const arr = new Array (1,2,3,4,5);
-const car = {type: "Ford", model:"500", color: "white"};
-console.log("Number : ",v1,"\tType : ",typeof(v1));
-console.log("String : ",v2,"\tType : ",typeof(v2));
-console.log("Boolean : ",b1,"\tType : ",typeof(b1));
-console.log("Array : ",arr,"\tType : ",typeof(arr));
-console.log("Object : ",car,"\tType : ",typeof(car));
+const car = {type: "Ford", model:"500", color: "white"}
+console.log("Number : ",v1,"\tType : ",typeof(v1))
+console.log("String : ",v2,"\tType : ",typeof(v2))
+console.log("Boolean : ",b1,"\tType : ",typeof(b1))
+console.log("Array : ",arr,"\tType : ",typeof(arr))
+console.log("Object : ",car,"\tType : ",typeof(car))
 
 // task5
 let myvar = 18
