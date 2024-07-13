0d49ba85f92e7661595457d21c8fbc799d91c15e
Aviral Sharma <154816352+avirals14@users.noreply.github.com>
2024-07-13 21:26:31 +0000

    order of task reassinged

--- Day01/program_01.js
+++ Day01/program_01.js
@@ -1,12 +1,13 @@
 // task1
 var v1 = 5;
 console.log(v1);
+// task2
 let v2 = "String";
 console.log(v2);
-// task2
+// task 3
 const b1 = true;
 console.log(b1);
-// task 3
+// task 4
 const arr = new Array (1,2,3,4,5);
 const car = {type: "Ford", model:"500", color: "white"};
 console.log("Number : ",v1,"\tType : ",typeof(v1));
@@ -15,8 +16,10 @@ console.log("Boolean : ",b1,"\tType : ",typeof(b1));
 console.log("Array : ",arr,"\tType : ",typeof(arr));
 console.log("Object : ",car,"\tType : ",typeof(car));
 
-// task4
+// task5
 let myvar = 18
 console.log("Initial : ",myvar)
 myvar = 7
 console.log("Final : ",myvar)
+
+// task6
