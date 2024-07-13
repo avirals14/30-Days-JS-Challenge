60d38ec86a28ed0be18a54729e7e2bbbcb9811fa
Aviral Sharma <154816352+avirals14@users.noreply.github.com>
2024-07-13 21:21:53 +0000

    till task3

--- Day01/program_01.js
+++ Day01/program_01.js
@@ -1,1 +1,16 @@
-console.log("Aviral");
+// task1
+var v1 = 5;
+console.log(v1);
+let v2 = "String";
+console.log(v2);
+// task2
+const b1 = true;
+console.log(b1);
+// task 3
+const arr = new Array (1,2,3,4,5);
+const car = {type: "Ford", model:"500", color: "white"};
+console.log("Number : ",v1,"\tType : ",typeof(v1));
+console.log("String : ",v2,"\tType : ",typeof(v2));
+console.log("Boolean : ",b1,"\tType : ",typeof(b1));
+console.log("Array : ",arr,"\tType : ",typeof(arr));
+console.log("Object : ",car,"\tType : ",typeof(car));
