65e6d273d6064cb79f8813d3f86c676f845ccf27
Aviral Sharma <154816352+avirals14@users.noreply.github.com>
2024-07-14 07:17:20 +0000

    code formatting

--- Day01/program_01.js
+++ Day01/program_01.js
@@ -1,15 +1,17 @@
+// Activity 1
 // task1
 var v1 = 5
 console.log(v1)
-
 // task2
 let v2 = "String"
 console.log(v2)
 
+// Activity 2
 // task 3
 const b1 = true
 console.log(b1)
 
+// Activity 3
 // task 4
 const arr = new Array (1,2,3,4,5);
 const car = {type: "Ford", model:"500", color: "white"}
@@ -19,12 +21,14 @@ console.log("Boolean : ",b1,"\tType : ",typeof(b1))
 console.log("Array : ",arr,"\tType : ",typeof(arr))
 console.log("Object : ",car,"\tType : ",typeof(car))
 
+// Activity 4
 // task5
 let myvar = 18
 console.log("Initial : ",myvar)
 myvar = 7
 console.log("Final : ",myvar)
 
+// Activity 5
 // task6
 const notvar = 45
 console.log(notvar)
