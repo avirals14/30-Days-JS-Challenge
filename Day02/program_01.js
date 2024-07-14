232d4d016f39344fc8bda1f70bb9f6d4fd95d3c5
Aviral Sharma <154816352+avirals14@users.noreply.github.com>
2024-07-14 07:15:06 +0000

    day02 completed

---
+++ Day02/program_01.js
@@ -0,0 +1,49 @@
+// Acitivity 1
+// task 1
+let a = 5
+let b = 2
+console.log("Sum : ",a+b)
+// task 2
+console.log("Difference : ",a-b)
+// task 3
+console.log("Product : ",a*b)
+// task 4
+console.log("Quotient : ",a/b)
+// task5
+console.log("Remainder : ",a%b)
+
+// Activity 2
+// task 6
+a+=b
+console.log("a+=b : ",a)
+// task 7
+a-=b
+console.log("a-=b : ",a)
+
+// Actitvity 3
+let c = 10
+let d = "10"
+// task 8
+console.log("> : ",a>b)
+console.log("< : ",a<b)
+// task 9
+console.log(">= : ",a>=b)
+console.log("<= : ",a<=b)
+// task 10
+console.log("== : ",c==d)
+console.log("=== : ",c===d)
+
+// Activity 4
+let age = 17
+// task 11
+console.log("&& : ", age>18 && age<67)
+// task 12
+console.log("|| : ", age>18 || age<67)
+// task 13
+console.log("! : ", !(age>18))
+
+// Activity 5
+// task 14
+let n = 7
+let ans = (n>0 ? "+ive" : n<0 ? "-ive" : "zero")
+console.log("n : ",ans)
