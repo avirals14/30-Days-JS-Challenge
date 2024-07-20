685372e21fc4496ee590783697b0e39fc4816c89
Aviral Sharma <154816352+avirals14@users.noreply.github.com>
2024-07-20 20:27:32 +0000

    Day06 Activity 2 copleted

---
+++ Day06/program_01.js
@@ -0,0 +1,24 @@
+// Activity 1
+// Task 1
+let a = [1,2,3,4,5]
+console.log(a)
+// Task 2
+console.log(`First : ${a[0]}\tSecond : ${a[4]}`)
+
+// Activity 2
+// Task 3
+a.push(6)
+console.log(a)
+// Task 4
+a.pop()
+console.log(a)
+// Task 5
+a.shift()
+console.log(a)
+// Task 6
+a.unshift(1)
+console.log(a)
+
+// Activity 3
+// Task 7
+let b = []
