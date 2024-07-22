22ea151e721600aad188720d39b6340f752fb333
Aviral Sharma <154816352+avirals14@users.noreply.github.com>
2024-07-22 16:19:41 +0000

    Day06 Completed

--- Day06/program_01.js
+++ Day06/program_01.js
@@ -1,24 +1,53 @@
 // Activity 1
 // Task 1
-let a = [1,2,3,4,5]
+console.log("Activity 1 : ")
+const a = [1,2,3,4,5]
 console.log(a)
 // Task 2
 console.log(`First : ${a[0]}\tSecond : ${a[4]}`)
 
 // Activity 2
 // Task 3
+console.log("\nAcitivity 2 : ")
 a.push(6)
-console.log(a)
+console.log("Push : ",a)
 // Task 4
 a.pop()
-console.log(a)
+console.log("Pop : ",a)
 // Task 5
 a.shift()
-console.log(a)
+console.log("Shift : ",a)
 // Task 6
 a.unshift(1)
-console.log(a)
+console.log("Unshift : ",a)
 
 // Activity 3
 // Task 7
-let b = []
+const b = a.map((x) => 2*x)
+console.log("\nActivity 3 : \nMap : ",b)
+// Task 8
+const c = a.filter((x) => x%2==0)
+console.log("Filter : ",c);
+// Task 9
+const d = a.reduce((a,b,i) => a+b)
+console.log("Reduce (Sum): ",d)
+
+// Activity 4
+// Task 10
+console.log("Activity 4 : \nFor : ")
+for(let i=0; i<a.length; i++){
+    console.log(a[i]);
+}
+// Task 11
+console.log("ForEach : ")
+a.forEach(i => {
+    console.log(i)
+});
+
+// Activity 5
+// Task 12
+console.log("Activity 5 : ")
+const twoD = [[1,2], [3,4], [5,6]]
+console.log("2-D Array : ",twoD)
+// Task 13
+console.log("twoD[0][1] : ",twoD[0][1])
